5bcf47f13048aaa65639f6c940e977d429b4f7b0
arthurmelob <49168524+arthurmelob@users.noreply.github.com>
2019-11-21 23:24:15 -0300

    added photos and functions(in process)

--- main.js
+++ main.js
@@ -106,4 +106,136 @@ perfil.addEventListener('click', function(event){
         menuAcc.style.display = 'none'
     }
     console.log(event)
+});
+
+
+// popular slide //
+
+
+let slides = [
+    {
+        popular1: 'assets/popular/vikingsWrapper.jpg',
+        popular2: 'assets/popular/gothamWrapper.jpg',
+        popular3: 'assets/popular/breakingbadWrapper.jpg',
+        popular4: 'assets/popular/brooklyn99Wrapper.jpg',
+        list1: 'assets/mylist/daredevilWrapper.png',
+        list2: 'assets/mylist/blacksailsWrapper.png',
+        list3: 'assets/mylist/blacklistWrapper.png',
+        list4: 'assets/mylist/theranchWrapper.png',
+        again1: 'assets/watchagain/spartacusWrapper.jpg',
+        again2: 'assets/watchagain/punisherWrapper.jpg',
+        again3: 'assets/watchagain/sherlockWrapper.jpg',
+        again4: 'assets/watchagain/freshprinceWrapper.jpg'
+    },
+    {
+        popular1: 'assets/popular/',
+        popular2: 'assets/popular/',
+        popular3: 'assets/popular/',
+        popular4: 'assets/popular/',
+        list1: 'assets/mylist/',
+        list2: 'assets/mylist/',
+        list3: 'assets/mylist/',
+        list4: 'assets/mylist/',
+        again1: 'assets/watchagain/',
+        again2: 'assets/watchagain/',
+        again3: 'assets/watchagain/',
+        again4: 'assets/watchagain/'
+    }  
+]
+
+let counter = 0
+let popular1 = document.querySelector('.popular1')
+let popular2 = document.querySelector('.popular2')
+let popular3 = document.querySelector('.popular3')
+let popular4 = document.querySelector('.popular4')
+let list1 = document.querySelector('.list1')
+let list2 = document.querySelector('.list2')
+let list3 = document.querySelector('.list3')
+let list4 = document.querySelector('.list4')
+let again1 = document.querySelector('.again1')
+let again2 = document.querySelector('.again2')
+let again3 = document.querySelector('.again3')
+let again4 = document.querySelector('.again4')
+
+function setImage(){
+    popuplar1.setAttribute('src', slides[counter].popular1)
+    popuplar2.setAttribute('src', slides[counter].popular2)
+    popuplar3.setAttribute('src', slides[counter].popular3)
+    popuplar4.setAttribute('src', slides[counter].popular4)
+    list1.setAttribute('src', slider[counter].list1)
+    list2.setAttribute('src', slider[counter].list2)
+    list3.setAttribute('src', slider[counter].list3)
+    list4.setAttribute('src', slider[counter].list4)
+    again1.setAttribute('src', slides[counter].again1)
+    again2.setAttribute('src', slides[counter].again2)
+    again3.setAttribute('src', slides[counter].again3)
+    again4.setAttribute('src', slides[counter].again4)
+};
+
+let prevImg1 = document.querySelector('.prevArrow1')
+let prevImg2 = document.querySelector('.prevArrow2')
+let prevImg3 = document.querySelector('.prevArrow3')
+let nextImg1 = document.querySelector('.nextArrow1')
+let nextImg2 = document.querySelector('.nextArrow2')
+let nextImg3 = document.querySelector('.nextArrow3')
+
+prevImg1.addEventListener('click', function(){
+    if(counter > 0){
+        counter--
+    }
+    else{
+        counter = 1
+    }
+    setImage()
 })
+nextImg1.addEventListener('click', function(){
+    if(counter < 1){
+        counter++
+    }
+    else{
+        counter = 0
+    }
+    setImage()
+});
+
+
+prevImg2.addEventListener('click', function(){
+    if(counter > 0){
+        counter--
+    }
+    else{
+        counter = 1
+    }
+    setImage()
+})
+nextImg2.addEventListener('click', function(){
+    if(counter < 1){
+        counter++
+    }
+    else{
+        counter = 0
+    }
+    setImage()
+});
+
+
+prevImg3.addEventListener('click', function(){
+    if(counter > 0){
+        counter--
+    }
+    else{
+        counter = 1
+    }
+    setImage()
+})
+nextImg3.addEventListener('click', function(){
+    if(counter < 1){
+        counter++
+    }
+    else{
+        counter = 0
+    }
+    setImage()
+})
+
+// // 
